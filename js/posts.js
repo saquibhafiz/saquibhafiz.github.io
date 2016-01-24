8bb1c9ccdd95c8db255bacb1e4cb776672c54364
Saquib Hafiz <hafiz.saquib@gmail.com>
2016-01-23 23:39:18 -0500

    generated site

---
+++ js/posts.js
@@ -0,0 +1,41 @@
+var posts = [
+    {
+        "title": "Base Negative Two (BNT): Part 2 - Negation, Addition and Subtraction",
+        "subtitle": "Basic math with BNT and generic neagative base systems",
+        "date": "December 7th 2014",
+        "imageID": "5068845349",
+        "entry": "bnt_part_2.html"
+    },
+
+    {
+        "title": "Base Negative Two (BNT): Part 1 - A Simple Understanding",
+        "subtitle": "Number systems and the power of negative based number systems",
+        "date": "November 7th 2014",
+        "imageID": "5068845349",
+        "entry": "bnt_part_1.html"
+    },
+
+    {
+        "title": "Interview Question: The power of binary search",
+        "subtitle": "How to transform basic algorithms into powerful ones",
+        "date": "November 5th 2014",
+        "imageID": "2913113348",
+        "entry": "interview_question_the_power_of_binary_search.html"
+    },
+
+    {
+        "title": "Interview Question: Poisoned Prisoners",
+        "subtitle": "How I broke down one problem into another",
+        "date": "March 29th 2014",
+        "imageID": "300296374",
+        "entry": "interview_question_poisoned_prisoners.html"
+    },
+
+    {
+        "title": "The Good, The Bad and The Web",
+        "subtitle": "Things I wish I could change about webdev",
+        "date": "March 10th 2014",
+        "imageID": "10099317756",
+        "entry": "the_good_the_bad_and_the_web.html"
+    }
+];
